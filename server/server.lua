533255567c6573ef18fb8f3bd9926f1793ddf34a
Froot <94587874+FrootLoopsBowl@users.noreply.github.com>
2023-03-08 20:28:16 -0500

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -8,7 +8,7 @@ RegisterCommand("annonces", function(source, args, rawCommand)
         if xPlayer.getGroup() == v then
             local text = table.concat(args, " ")
             TriggerClientEvent("Froot:Scaleform", -1, text)
-            PerformHttpRequest("https://discord.com/api/webhooks/1008947704937840640/famR-ASU-xnpH7MZdavXlC1JI2UnrivnTwMc8Lq9d-k27VF_F2oredpG83BDFQwwYZ-B", function(err, text, headers) end, 'POST', json.encode({username = "Annonces Staff", avatar_url = "", content = "Le joueur **"..GetPlayerName(source).."** a envoyé une annonce : **"..text.."**"}), { ['Content-Type'] = 'application/json' })
+            PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({username = "Annonces Staff", avatar_url = "", content = "Le joueur **"..GetPlayerName(source).."** a envoyé une annonce : **"..text.."**"}), { ['Content-Type'] = 'application/json' })
         end
     end
 end, false)
