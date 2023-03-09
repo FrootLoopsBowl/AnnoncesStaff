6e0262ed89ee6d3cce930ba88d915fcf85c4298e
Froot <94587874+FrootLoopsBowl@users.noreply.github.com>
2023-03-08 20:28:44 -0500

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,5 +1,6 @@
 Config = {
     ESX = "esx:getSharedObject", --::{korioz#0110}::esx:getSharedObject si vous utilisez une base california
+    Webhook = "", --Webhook pour les logs discord
     Title = "Annonces", --Titre de l'annonce
     Color = "~r~", -- Pour les couleurs https://iqcode.com/code/other/fivem-color-codes
     Time = 10, --Temps en seconde de l'annonce
